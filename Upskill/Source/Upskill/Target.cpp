6e003962e1c2bd446214d36af503b94f0231c9dc
Xaqqa <35543794+Xaqqa@users.noreply.github.com>
2021-10-29 14:28:58 +0100

    Added Point Gaining

--- Upskill/Source/Upskill/Target.cpp
+++ Upskill/Source/Upskill/Target.cpp
@@ -26,7 +26,7 @@ void UTarget::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Player = GetWorld()->GetFirstPlayerController()->GetPawn()->GetOwner()->FindComponentByClass<UMovement>();
+	Player = Cast<UMovement>(GetWorld()->GetFirstPlayerController()->GetPawn()->GetOwner()->FindComponentByClass<UMovement>());
 	SphereCollision = GetOwner()->FindComponentByClass<USphereComponent>();
 	TargetAnchor = GetOwner()->FindComponentByClass<UArrowComponent>();
 
